00fcb6ad18f625253a836e2996b1e7e1eadd85e3
Luisito Canlas <luisitocanlas@gmail.com>
2024-04-04 16:27:42 -0700

    Added functions and event listener to the button

--- scripts/script.js
+++ scripts/script.js
@@ -10,23 +10,49 @@ const alternateBgColor = '#669BBC';
 let cellSize = 16;
 let GRID_SIZE = cellSize ** 2;
 
-for (let i = 0; i < GRID_SIZE; i++) {
-	let cell = document.createElement('div');
-	cell.classList.add('cell');
-	cell.setAttribute(
-		'style',
-		`width: ${containerWidth / cellSize}px; 
+btn.addEventListener('click', () => {
+	cellSize = prompt('Please enter a number (1-100):');
+
+	if (cellSize === null) {
+		return;
+	}
+
+	cellSize = parseInt(cellSize);
+
+	if (isNaN(cellSize) || cellSize <= 0 || cellSize > 100) {
+		alert('Please enter a number between 1 - 100.');
+	} else {
+		removeGrid();
+		GRID_SIZE = cellSize ** 2;
+		generateGrid();
+	}
+});
+
+generateGrid();
+
+function generateGrid() {
+	for (let i = 0; i < GRID_SIZE; i++) {
+		let cell = document.createElement('div');
+		cell.classList.add('cell');
+		cell.setAttribute(
+			'style',
+			`width: ${containerWidth / cellSize}px; 
     height: ${containerHeight / cellSize}px;`
-	);
+		);
 
-	cell.addEventListener('mouseenter', () => {
-		cell.style.backgroundColor = alternateBgColor;
-	});
+		cell.addEventListener('mouseenter', () => {
+			cell.style.backgroundColor = alternateBgColor;
+		});
 
-	cell.addEventListener('mouseleave', () => {
-		cell.style.transition = 'background-color 0.7s ease';
-		cell.style.backgroundColor = baseBgColor;
-	});
+		cell.addEventListener('mouseleave', () => {
+			cell.style.transition = 'background-color 0.7s ease';
+			cell.style.backgroundColor = baseBgColor;
+		});
 
-	container.appendChild(cell);
+		container.appendChild(cell);
+	}
+}
+
+function removeGrid() {
+	container.innerHTML = '';
 }
