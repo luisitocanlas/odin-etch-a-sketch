a5c694e0372e7fbbc13d088a3de98fce9895fc89
Luisito Canlas <luisitocanlas@gmail.com>
2024-04-04 14:32:15 -0700

    Created a 16x16 grid of square divs

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 const container = document.querySelector('#container');
+
+const containerWidth = 960;
+const containerHeight = 960;
+
+let cellSize = 16;
+let GRID_SIZE = cellSize ** 2;
+
+for (let i = 0; i < GRID_SIZE; i++) {
+	let cell = document.createElement('div');
+	cell.classList.add('cell');
+	cell.setAttribute(
+		'style',
+		`width: ${containerWidth / cellSize}px; 
+    height: ${containerHeight / cellSize}px;`
+	);
+
+	container.appendChild(cell);
+}
